3e231651bb4e0c10a2b668db6d4fe69301bc253e
Rafal Sroka <rafal.sroka.it@gmail.com>
2014-02-15 19:02:47 +0100

    Using blocks for animations.

--- SpriteAnimationDemo/SpriteAnimationDemo/MasterViewController.m
+++ SpriteAnimationDemo/SpriteAnimationDemo/MasterViewController.m
@@ -47,7 +47,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
     [self setTitle:NSLocalizedString(@"Sprite Animation", @"")];
     [self switchValueChanged:customRangeSwitch];
 }
@@ -68,7 +68,7 @@
     }
     
     //
-    // This cool sprite sheet can be found at http://gushh.net/blog/free-game-sprites-explosion-3/ 
+    // This cool sprite sheet can be found at http://gushh.net/blog/free-game-sprites-explosion-3/
     // I added numbers to this image to make testing and debuging easier.
     //
     UIImage *spriteSheet = [UIImage imageNamed:@"explosion_4_39_128_debug"];
@@ -78,24 +78,24 @@
         NSRange range = NSMakeRange([locationLabel.text intValue],
                                     [lengthLabel.text intValue]);
         
-        NSArray *arrayWithSprites = [spriteSheet spritesWithSpriteSheetImage:spriteSheet 
-                                                                     inRange:range 
+        NSArray *arrayWithSprites = [spriteSheet spritesWithSpriteSheetImage:spriteSheet
+                                                                     inRange:range
                                                                   spriteSize:CGSizeMake(128, 128)];
         [imageView setAnimationImages:arrayWithSprites];
     }
     else
     {
-        NSArray *arrayWithSprites = [spriteSheet spritesWithSpriteSheetImage:spriteSheet 
+        NSArray *arrayWithSprites = [spriteSheet spritesWithSpriteSheetImage:spriteSheet
                                                                   spriteSize:CGSizeMake(128, 128)];
         [imageView setAnimationImages:arrayWithSprites];
     }
     
     NSLog(@"Sprite images: %i", [imageView.animationImages count]);
-
+    
     float animationDuration = [imageView.animationImages count] * 0.100; // 100ms per frame
     
     [imageView setAnimationRepeatCount:0];
-    [imageView setAnimationDuration:animationDuration]; 
+    [imageView setAnimationDuration:animationDuration];
     [imageView startAnimating];
     
     [((UIButton *)sender) setTitle:NSLocalizedString(@"Stop animation!", @"")
@@ -119,19 +119,19 @@
 {
     UISwitch *sw = (UISwitch *)sender;
     [settingsView setUserInteractionEnabled:[sw isOn]];
-
-    [UIView beginAnimations:nil context:nil];
     
-    if ([sw isOn])
-    {
-        [settingsView setAlpha:1.0];
-    }
-    else
-    {
-        [settingsView setAlpha:0.2];
-    }
-    
-    [UIView commitAnimations];
+    [UIView animateWithDuration:1.0f
+                     animations:^
+     {
+         if ([sw isOn])
+         {
+             [settingsView setAlpha:1.0];
+         }
+         else
+         {
+             [settingsView setAlpha:0.2];
+         }
+     }];
 }
 
 
